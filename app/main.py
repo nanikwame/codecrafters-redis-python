3c2cf4184dbb1a35b96017ef8d42e050671270b2
Kwame Nani <vertify@Kwames-MacBook-Pro.local>
2023-02-26 09:19:56 -0500

    respond to ping

--- app/main.py
+++ app/main.py
@@ -9,8 +9,13 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() # wait for client
 
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.send("+PONG\r\n".encode())
 
 if __name__ == "__main__":
     main()
